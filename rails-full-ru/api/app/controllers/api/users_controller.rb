e9954e4d53400107c316fc9c8f1485fcaa54ebcc
@hexlet/cli <support@hexlet.io>
2024-03-02 14:15:50 +0300

    submit rails-full-ru/api

---
+++ rails-full-ru/api/app/controllers/api/users_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Api::UsersController < Api::ApplicationController
+  def index
+    @users = User.all
+    respond_with @users.to_json
+  end
+
+  def show
+    @user = User.find params[:id]
+
+    respond_with @user.to_json
+  end
+end
