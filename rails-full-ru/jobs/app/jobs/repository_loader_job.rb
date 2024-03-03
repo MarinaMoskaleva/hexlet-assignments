8eaa1bc9a0a141580e137b7b79e0a93993a99bc2
@hexlet/cli <support@hexlet.io>
2024-03-03 20:00:36 +0300

    submit rails-full-ru/jobs

--- rails-full-ru/jobs/app/jobs/repository_loader_job.rb
+++ rails-full-ru/jobs/app/jobs/repository_loader_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RepositoryLoaderJob < ApplicationJob
   queue_as :default
 
@@ -10,15 +12,15 @@ class RepositoryLoaderJob < ApplicationJob
     github_data = client.repository(octokit_repo)
 
     if repository.update!(
-        repo_name: github_data[:name],
-        owner_name: github_data[:owner][:login],
-        description: github_data[:description],
-        default_branch: github_data[:default_branch],
-        watchers_count: github_data[:watchers_count],
-        language: github_data[:language],
-        repo_created_at: github_data[:created_at],
-        repo_updated_at: github_data[:updated_at]
-      )
+      repo_name: github_data[:name],
+      owner_name: github_data[:owner][:login],
+      description: github_data[:description],
+      default_branch: github_data[:default_branch],
+      watchers_count: github_data[:watchers_count],
+      language: github_data[:language],
+      repo_created_at: github_data[:created_at],
+      repo_updated_at: github_data[:updated_at]
+    )
       repository.finish_fetching!
     else
       repository.fail!
